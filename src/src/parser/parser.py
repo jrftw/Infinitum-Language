48857053799fcc01ad6122c6bbd32c2a0b386e1a
Junior <68521496+jrftw@users.noreply.github.com>
2024-12-05 00:54:08 -0500

    Create parser.py

---
+++ src/src/parser/parser.py
@@ -0,0 +1,8 @@
+class Parser:
+    def __init__(self, tokens):
+        self.tokens = tokens
+
+    def parse(self):
+        # Basic placeholder for parsing logic
+        ast = {"program": self.tokens}
+        return ast
